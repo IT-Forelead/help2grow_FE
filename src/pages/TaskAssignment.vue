86959c84e4c5440d88ff8797c352c4b039011f0a
ogabek001 <ogabek@bulavka.uz>
2023-10-15 02:25:43 +0500

    imp: task badge added

--- src/pages/TaskAssignment.vue
+++ src/pages/TaskAssignment.vue
@@ -33,10 +33,18 @@ const lorem = new LoremIpsum({
             {{ s }}
           </div>
         </div>
-        <div class="grid max-h-screen grid-cols-2 gap-3 mt-5 overflow-auto">
-          <div v-for="(i, idx) in Math.floor(Math.random() * 50)" :key="idx" class="p-1 border rounded-lg h-fit">
+        <div class="flex flex-wrap max-h-screen gap-3 mt-5 overflow-auto">
+          <div v-for="(i, idx) in Math.floor(Math.random() * 50)" :key="idx" class="w-full p-1 border rounded-lg h-fit">
             <h1 class="font-bold">{{ lorem.generateSentences(1) }}</h1>
-            <p>{{ lorem.generateWords(i) }}</p>
+            <p>{{ lorem.generateWords(Math.floor(Math.random() * 20)) }}</p>
+            <div>
+              <span
+                class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">{{ lorem.generateWords(1) }}</span>
+              <span
+                class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">{{ lorem.generateWords(1) }}</span>
+              <span
+                class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">{{ lorem.generateWords(1) }}</span>
+            </div>
           </div>
         </div>
       </div>
