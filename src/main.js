59760df2632062f66742ee8a0d6d27ed980463cb
ogabek001 <ogabek@bulavka.uz>
2023-10-14 11:25:52 +0500

    feat: login and dashboard layout added

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './style.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import router from "./router";
+const pinia = createPinia();
+import App from "./App.vue";
+
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+app.mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../stores/auth.store";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  routes: [
+    {
+      path: "/",
+      redirect: "/dashboard",
+      component: () => import("../layouts/Default.vue"),
+      children: [
+        {
+          path: "/dashboard",
+          name: "Dashboard",
+          component: () => import("../pages/Dashboard.vue"),
+          meta: {
+            title: "Asosiy",
+            protected: true,
+          },
+        },
+      ],
+    },
+    {
+      path: "/auth",
+      redirect: "/auth/login",
+      component: () => import("../layouts/Auth.vue"),
+      children: [
+        {
+          path: "/auth/login",
+          name: "Login",
+          component: () => import("../pages/Login.vue"),
+          meta: {
+            title: "Tizimga kirish",
+          },
+        },
+      ],
+    },
+  ],
+});
+
+router.beforeEach(async (to, from, next) => {
+  document.title = `${to.meta.title} | Help to grow`;
+  const authStore = useAuthStore();
+  const isAuthenticated = authStore.user !== null;
+  if (to.meta.protected && !isAuthenticated) {
+    next({ name: "Login" });
+  } else {
+    next();
+  }
+});
+
+export default router;
