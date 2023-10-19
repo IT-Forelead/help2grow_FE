32ff958571c37b866f4f8ef76e34e862acce43fa
OG'ABEK <shomoyxhacker007@mail.ru>
2023-10-19 13:44:57 +0500

    imp: TA section

--- src/pages/TaskAssignment.vue
+++ src/pages/TaskAssignment.vue
@@ -2,22 +2,9 @@
 import { computed } from 'vue';
 import JuniorTACard from '../components/must/JuniorTACard.vue';
 import { useMMStore } from '../stores/mm.store';
-import { LoremIpsum } from "lorem-ipsum";
 
+const tasks = computed(() => useMMStore().tasks?.reverse())
 const taJunior = computed(() => useMMStore().taJunior)
-
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
-
-
 
 </script>
 <template>
@@ -33,17 +20,21 @@ const lorem = new LoremIpsum({
           </div>
         </div>
         <div class="flex flex-wrap max-h-screen gap-3 mt-5 overflow-auto">
-          <div v-for="(i, idx) in Math.floor(Math.random() * 50)" :key="idx" class="w-full p-1 border rounded-lg h-fit">
-            <h1 class="font-bold">{{ lorem.generateSentences(1) }}</h1>
-            <p>{{ lorem.generateWords(Math.floor(Math.random() * 20)) }}</p>
-            <div>
-              <span
-                class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">{{ lorem.generateWords(1) }}</span>
-              <span
-                class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">{{ lorem.generateWords(1) }}</span>
-              <span
-                class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">{{ lorem.generateWords(1) }}</span>
-            </div>
+          <div v-for="(t, idx) in tasks" :key="idx"
+            class="w-full p-2 border rounded-lg cursor-pointer h-fit hover:border-green-400">
+            <h1 class="font-bold">{{ t?.id }}</h1>
+            <h1 class="font-bold">{{ t?.title }}</h1>
+            <p>{{ t?.desc }}</p>
+            <span v-for="(tt, idx) in t?.tags" :key="idx">
+              <span v-if="idx % 2 === 0"
+                class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+                {{ tt }}
+              </span>
+              <span v-else
+                class="bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">
+                {{ tt }}
+              </span>
+            </span>
           </div>
         </div>
       </div>
